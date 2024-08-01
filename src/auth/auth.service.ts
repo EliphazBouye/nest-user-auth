71b1d482a7db9806a0f7533f9c5f96d6c94914e5
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-08-01 08:39:16 +0000

    Ready. Set. Go.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import assert from 'assert';
+import { UsersService } from 'src/users/users.service';
+
+type UserLoginInfo = {
+  username: string,
+  password: string,
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}  
+
+  async signIn(userLoginInfo: UserLoginInfo): Promise<any> {
+    assert(userLoginInfo.username, "Username should be define");
+    assert(userLoginInfo.password, "Password should be define");
+    
+    try {
+	const user = await this.usersService.user({
+	    username: userLoginInfo.username
+	});
+      if (user.password !== userLoginInfo.password ) {
+          throw new UnauthorizedException()
+      }
+
+        const { password, ...result } = user;
+        return result;
+        
+    } catch (err) {
+	console.error(err);
+    }
+
+  }
+  
+}
