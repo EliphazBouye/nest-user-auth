5b380c80f2a745cccae46b48409208603919348a
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-08-01 16:11:49 +0000

    fix typo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,22 +14,19 @@ export class AuthService {
   async signIn(userLoginInfo: UserLoginInfo): Promise<any> {
     assert(userLoginInfo.username, "Username should be define");
     assert(userLoginInfo.password, "Password should be define");
-    
-    try {
-	const user = await this.usersService.user({
-	    username: userLoginInfo.username
-	});
-      if (user.password !== userLoginInfo.password ) {
-          throw new UnauthorizedException()
-      }
 
+    try {
+        const user = await this.usersService.user({
+            username: userLoginInfo.username
+        });
+        if (user.password !== userLoginInfo.password ) {
+            throw new UnauthorizedException()
+        }
         const { password, ...result } = user;
         return result;
-        
     } catch (err) {
 	console.error(err);
     }
-
   }
-  
+
 }
